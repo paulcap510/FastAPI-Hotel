6d0cde6b90cbfa295b9c74a81c0c048c7aa89324
Paul <paulcapob@gmail.com>
2024-08-31 12:04:22 +0900

    Updated

--- app/routers/users.py
+++ app/routers/users.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from sqlalchemy.orm import Session
 from fastapi.responses import JSONResponse
 from ..database import get_db
-from ..schemas import UserCreate, UserRead  # Added UserRead
+from ..schemas import UserCreate, UserRead  
 from ..models import User
 from ..utils.auth import hash_password, generate_sha256_hash, get_user_by_email
 # from ..utils.email import send_signup_email  # Commented out for now
@@ -11,7 +11,6 @@ import redis
 
 router = APIRouter()
 
-# Initialize Redis
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 @router.post("/register", response_model=UserRead)
@@ -59,21 +58,16 @@ async def register_by_email(
     return JSONResponse(status_code=200, content=jsonMsg)
 
 
-
-# @router.post("/register_by_email", response_model=UserRead, status_code=status.HTTP_201_CREATED)
-# def register_by_email(user: UserCreate, db: Session = Depends(get_db)):
-#     db_user = db.query(User).filter(User.email == user.email).first()
-#     if db_user:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
-
-#     hashed_password = hash_password(user.password.get_secret_value())
-#     new_user = User(email=user.email, hashed_password=hashed_password, is_active=False)  # Initially inactive
-#     db.add(new_user)
-#     db.commit()
-#     db.refresh(new_user)
-
-#     # Generate verification token and send verification email
-#     token = create_verification_token(new_user.email)
-#     send_verification_email(new_user.email, token)
-
-#     return {"message": "User registered. Please check your email to verify your account."}
+@router.get("/verify-email/")
+async def verify_email(token: str, db: Session = Depends(get_db)):
+    user_email = redis_client.get(f"signup_token_for_{token}")
+    if not user_email:
+        raise HTTPException(status_code=400, detail="Invalid or expired token.")
+    
+    user = get_user_by_email(db, user_email.decode("utf-8"))
+    if user:
+        user.is_active = True  
+        db.commit()
+        return {"message": "Email verified successfully! Your account is now active."}
+    else:
+        raise HTTPException(status_code=404, detail="User not found.")

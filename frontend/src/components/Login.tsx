929e5e2cca699f5a58d9adcb7bc6626e498ada13
Paul <paulcapob@gmail.com>
2024-09-01 16:56:26 +0900

    Created login component

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = async (e: React.FormEvent) => {};
+
+  return (
+    <div>
+      <div className="login-div">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
